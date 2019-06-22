f30ddd15b3c65112a1fd9e3958b65ad7e4975661
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 00:17:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,19 +9,3 @@ new_array = []
 new_array
 end
  
-=======
-  
-  while i < collection.length 
-  yield collection[i]
-  i= i + 1 
-  end
-  collection.collect do |coll|
-    coll.split(" ").first
-  end
-  collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
-end
-
->>>>>>> 32882198b45b466091ba00608c815fcfee60631e
